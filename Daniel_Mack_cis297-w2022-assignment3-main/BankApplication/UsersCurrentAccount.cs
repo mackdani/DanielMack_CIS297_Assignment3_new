c457c3701ac9b7a3e1b7cf2620c3312097363f0f
mackdani <98480193+mackdani@users.noreply.github.com>
2022-03-10 22:27:34 -0500

    Add files via upload

---
+++ Daniel_Mack_cis297-w2022-assignment3-main/BankApplication/UsersCurrentAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class UsersCurrentAccount : Account
+    {
+        public UsersCurrentAccount()
+        {
+            this.setInterestRate(0.15);
+            this.setAccounttype("Current");
+        }
+        private void CancelInterest()
+        {
+            double balance = BalanceDisplay();
+            double New_Balance = balance + (balance * getInterestRate());
+        }
+    }
+}
